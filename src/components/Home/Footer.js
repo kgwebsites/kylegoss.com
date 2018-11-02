afbb4aa81329a087819dc3ff4432cdee96c2f844
Kyle Goss <kyle@kgwebsites.com>
2018-11-01 23:13:53 -0700

    Fixed initial page load jank and updated content

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -1,51 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const facebook = '/static/images/fb.svg';
-const instagram = '/static/images/ig.svg';
+// const facebook = '/static/images/fb.svg';
+// const instagram = '/static/images/ig.svg';
 const linkedin = '/static/images/li.svg';
 const youtube = '/static/images/yt.svg';
 const github = '/static/images/gh.svg';
-const reddit = '/static/images/rd.svg';
+// const reddit = '/static/images/rd.svg';
 
 const socialProfiles = [
   {
-    social: facebook,
-    url: 'https://www.facebook.com/gossisboss1',
-  },
-  {
-    social: instagram,
-    url: 'https://www.instagram.com/gossb0ss/',
-  },
-  {
     social: linkedin,
-    url: 'https://www.linkedin.com/in/gosskyle/',
+    url: 'https://www.linkedin.com/in/gosskyle/'
   },
   {
     social: youtube,
-    url: 'https://www.youtube.com/c/WebDevProfesh',
+    url: 'https://www.youtube.com/c/WebDevProfesh'
   },
   {
     social: github,
-    url: 'https://github.com/kgwebsites',
-  },
-  {
-    social: reddit,
-    url: 'https://www.reddit.com/user/kgwebsites',
-  },
+    url: 'https://github.com/kgwebsites'
+  }
 ];
 
 const FooterContainer = styled.footer`
-    display: flex;
-    padding: 4rem 0;
-    align-items: center;
-    justify-content: space-around;
-    background-color: #333;
+  display: flex;
+  padding: 4rem 0;
+  align-items: center;
+  justify-content: space-around;
+  background-color: #333;
 `;
 
 const Footer = () => (
   <FooterContainer>
-    {socialProfiles.map(social => <a key={`${social.url}-${social.social}`} href={social.url} target="_blank"><img src={social.social} height="25" alt={social.social} /></a>)}
+    {socialProfiles.map(social => (
+      <a
+        key={`${social.url}-${social.social}`}
+        href={social.url}
+        target="_blank"
+      >
+        <img src={social.social} height="25" alt={social.social} />
+      </a>
+    ))}
   </FooterContainer>
 );
 
--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-import { KeyboardArrowDown } from 'material-ui-icons';
 
 const Kyle = '/static/images/kyle-jason-goss.jpg';
 
 const FullScreen = styled.section`
-    display: flex;
-    height: 100vh;
-    width: 100vw;
-    background: url(${Kyle}) 100% 100%/cover no-repeat fixed;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-`;
-
-const KeyboardArrowDownStyled = styled(KeyboardArrowDown)`
-    font-size: 2rem;
-    text-shadow: 1px 1px 1px #fff;
-    width: 3rem!important;
-    height: 3rem!important;
-    filter: drop-shadow( 0px 4px 0px #fff );
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  background: url(${Kyle}) 100% 100% / cover no-repeat fixed;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Hero = () => (
@@ -30,7 +21,6 @@ const Hero = () => (
         <h1>Kyle Goss</h1>
         <h2>Software Engineer</h2>
       </div>
-      <KeyboardArrowDownStyled />
     </FullScreen>
   </section>
 );
