b845c8a2e95fbc313d2d34269d1c32a39af9b921
Kyle Goss <kyle@kgwebsites.com>
2019-08-22 21:54:34 -0700

    v3

--- apps.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Resume from './apps/Resume';
-
-const resumeNode = document.getElementById('ResumeApp');
-ReactDOM.render(<Resume />, resumeNode);
